fc791f58381fe8d9ce9cecebb646eaa1d247f285
Никита Волков <n_volkov@head-point.ru>
2023-02-06 22:24:36 +0300

    change stack to .net core

---
+++ BirthdayBot/Core/Services/ReceiverService.cs
@@ -0,0 +1,16 @@
+using BirthdayBot.Core.Abstract;
+using Telegram.Bot.Abstract;
+
+namespace Telegram.Bot.Services;
+
+// Compose Receiver and UpdateHandler implementation
+public class ReceiverService : ReceiverServiceBase<UpdateHandler>
+{
+    public ReceiverService(
+        ITelegramBotClient botClient,
+        UpdateHandler updateHandler,
+        ILogger<ReceiverServiceBase<UpdateHandler>> logger)
+        : base(botClient, updateHandler, logger)
+    {
+    }
+}
